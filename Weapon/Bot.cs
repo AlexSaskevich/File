25a9782e2353679f46c00c57977cf49c083a6177
AlexSaskevich <saskevich.aleksey@gmail.com>
2022-06-28 16:45:44 +0300

    Improved Encapsulation

---
+++ Weapon/Bot.cs
@@ -0,0 +1,17 @@
+﻿namespace Weapon
+{
+    internal class Bot
+    {
+        public Bot()
+        {
+            Weapon = new Weapon();
+        }
+
+        public Weapon Weapon { get; private set; }
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.TryFire(player);
+        }
+    }
+}
---
+++ Weapon/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Weapon
+{
+    internal class Weapon
+    {
+        private readonly int _damage;
+
+        public Weapon(int damage = 10, int bullets = 1)
+        {
+            if (damage <= 0 || bullets <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            _damage = damage;
+            Bullets = bullets;
+        }
+
+        public int Bullets { get; private set; }
+
+        public void TryFire(Player player)
+        {
+            if (player == null)
+                throw new NullReferenceException();
+
+            if (Bullets <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            Bullets -= 1;
+
+            player.ApplyDamage(_damage);
+        }
+    }
+}
