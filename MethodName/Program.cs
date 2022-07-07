933c71c07ddcc542586bfda9f3af0cc20bd5f10f
AlexSaskevich <saskevich.aleksey@gmail.com>
2022-07-07 19:53:42 +0300

    Improved method name

---
+++ MethodName/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MethodName
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+
+        private static int Clamp(int calue, int min, int max)
+        {
+            if (calue < min)
+                return min;
+            else if (calue > max)
+                return max;
+            else
+                return calue;
+        }
+    }
+}
