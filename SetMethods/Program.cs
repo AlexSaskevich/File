0dea463e199d9c600a352bce22f2ac43faeb6e3a
AlexSaskevich <saskevich.aleksey@gmail.com>
2022-07-10 20:35:43 +0300

    Improved naming

---
+++ SetMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SetMethods
+{
+    public static void CreateObject()
+    {
+        //Создание объекта на карте
+    }
+
+    public static void GetRandomChance()
+    {
+        _chance = Random.Range(0, 100);
+    }
+
+    public static int CalculateSalary(int hoursWorked)
+    {
+        return _hourlyRate * hoursWorked;
+    }
+}
